a9992faf2fbe6e25399bede7cfa562264245b4cf
Yutaka Kobayashi <48763691+kobayashiyutaka@users.noreply.github.com>
2021-06-11 21:59:54 +0900

    Update insditcmodule.c, add .gitignore

--- insdictmodule.c
+++ insdictmodule.c
@@ -1,15 +1,37 @@
 #include <Python.h>
-#include "/home/yutaka/GitHub/cpython/Objects/dict-common.h"
+#include "../cpython/Objects/dict-common.h"
 //#include "dict-common.h"
 
 static PyObject *method_insdict(PyObject *self,PyObject *args){
+  int i;
+  Py_ssize_t ma_used;
+  uint8_t index_tbl_size;
+  Py_ssize_t dk_nentries;
   PyDictObject *d;
+  PyDictKeysObject *ma_keys;
+  PyObject **ma_values;
   /* Parse arguments */
   if(!PyArg_ParseTuple(args, "O", &d)){
     return NULL;
   }
+  
+  ma_used = d->ma_used;
+  ma_keys = d->ma_keys;
+  ma_values = d->ma_values;
 
-  printf("%d\n",d->ma_keys->dk_indices[0]);
+  index_tbl_size = ma_keys->dk_log2_size;
+  dk_nentries = ma_keys->dk_nentries;
+
+  printf("---- Internal state of dictionary ----\n");
+  printf("Number of items: \t%lld\n",ma_used);
+  printf("Size of index table: \t%d\n",index_tbl_size);
+  printf("Index table: ");
+  for(i=0;i<index_tbl_size;i++){
+    printf("%d ",d->ma_keys->dk_indices[i]);
+  }
+  printf("\n");
+  printf("dk_nentries: %I64d\n",dk_nentries);
+
   return Py_None;
 }
 
